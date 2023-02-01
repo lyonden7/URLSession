55a001f0851768ad0a826ae6f5043715914e1f06
Denis Vasilev <lyon_den@mail.ru>
2023-02-01 16:24:47 +0300

    add DataStore

--- URLSession/ViewControllers/MealDetailViewController.swift
+++ URLSession/ViewControllers/MealDetailViewController.swift
@@ -146,48 +146,15 @@ extension MealDetailViewController {
 extension MealDetailViewController {
     private func getIngredients() {
         guard let meal = mealDetails.first else { return }
-        let ingridients = [
-            meal.ingredient1,
-            meal.ingredient2,
-            meal.ingredient3,
-            meal.ingredient4,
-            meal.ingredient5,
-            meal.ingredient6,
-            meal.ingredient7,
-            meal.ingredient8,
-            meal.ingredient9,
-            meal.ingredient10,
-            meal.ingredient11,
-            meal.ingredient12,
-            meal.ingredient13,
-            meal.ingredient14,
-            meal.ingredient15
-        ]
-        
-        mealIngredients = ingridients.compactMap { $0 }
-            .filter { !$0.isEmpty }
+        DataStore.shared.getIngridients(from: meal) { mealIngridients in
+            self.mealIngredients = mealIngridients
+        }
     }
     
     private func getMeasures() {
         guard let meal = mealDetails.first else { return }
-        let measures = [
-            meal.measure1,
-            meal.measure2,
-            meal.measure3,
-            meal.measure4,
-            meal.measure5,
-            meal.measure6,
-            meal.measure7,
-            meal.measure8,
-            meal.measure9,
-            meal.measure10,
-            meal.measure11,
-            meal.measure12,
-            meal.measure13,
-            meal.measure14,
-            meal.measure15
-        ]
-        
-        mealMeasures = measures.compactMap { $0 }
+        DataStore.shared.getMeasures(from: meal) { mealMeasures in
+            self.mealMeasures = mealMeasures
+        }
     }
 }
